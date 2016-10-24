d6e5f697292618ff971b2625254de5044a22fb06
ahza64 <ahza64@yahoo.com>
2016-10-23 23:53:54 -0700

    setting up external api and troubleshooting .env in bundler

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,11 +1,27 @@
 var axios = require('axios');
 
-require('dotenv').load();
+// add secret api key
+require('dotenv').config();
 
-const OPEN_WEATHER_MAP_URL = '';
+//build base url
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=' + 'process.env.WEATHER_KEY';
+console.log(OPEN_WEATHER_MAP_URL);
 
 module.exports = {
   getTemp: function (location) {
+    var encodedLocation = encodedURIComponent(location);
+    // add var params to url
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
+    // make external api req
+    axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
   }
 }
