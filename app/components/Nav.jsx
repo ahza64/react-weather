40ae2b35b2a4f6c783bcb635850fe396d0f4140e
ahza64 <ahza64@yahoo.com>
2016-10-25 23:00:44 -0700

    refactor stateless components with arrow functions

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,19 +1,17 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <h2>Nav Component</h2>
-        {/* React Links for custom styles */}
-        {/* use IndexLink for nested routes */}
-        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-        <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-        <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-      </div>
-    );
-  }
-});
+var Nav = () => {
+  return (
+    <div>
+      <h2>Nav Component</h2>
+      {/* React Links for custom styles */}
+      {/* use IndexLink for nested routes */}
+      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+      <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+      <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+    </div>
+  );
+};
 
 module.exports = Nav;
