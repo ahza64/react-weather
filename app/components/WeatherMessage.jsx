812217e2b442da1233d81769c9c9a6cf04137326
ahza64 <ahza64@yahoo.com>
2016-10-25 23:04:39 -0700

    refactor es6 object destructuring inside the function parameters

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,8 +1,6 @@
 var React = require('react');
 
-var WeatherMessage = (props) => {
-  var {temp, location} = props;
-
+var WeatherMessage = ({temp, location}) => {
   return (
     <div>
       <h3>It is {temp} in {location}.</h3>
