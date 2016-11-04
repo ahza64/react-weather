2df44dcd4c3594ec8374f137844546173b377d04
ahza64 <ahza64@yahoo.com>
2016-11-04 13:25:16 -0700

    weather page styling

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -33,7 +33,7 @@ var Weather = React.createClass({
 
     function renderMessage () {
       if (isLoading) {
-        return <h3>Fetching weather...</h3>;
+        return <h3 className="text-center">Fetching weather...</h3>;
       } else if (temp && location) {
         return <WeatherMessage temp={temp} location={location}/>;
       }
@@ -41,9 +41,7 @@ var Weather = React.createClass({
 
     return (
       <div>
-        <h3>
-          weather component
-        </h3>
+        <h1 className="text-center">weather component</h1>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
       </div>
