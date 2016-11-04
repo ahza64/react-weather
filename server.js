dc827c8b75dfae31e541da64e7e4242d390e5a35
ahza64 <ahza64@yahoo.com>
2016-11-04 12:36:48 -0700

    typo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 // openWeather only runs on http, need to redirect https to http
 app.use(function (req, res, next) {
   if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('//http:' + req.hostname = req.url);
+    res.redirect('//http:' + req.hostname + req.url);
   } else {
     next();
   }
